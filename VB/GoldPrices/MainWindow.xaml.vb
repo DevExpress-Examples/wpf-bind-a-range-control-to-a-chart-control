3d0fc5d044a951ad33d71b748181ce6308285eaf
DevExpressExampleBot <fakeEmail>
2023-12-12 16:01:39 +0400

    autogenerate VB

--- VB/GoldPrices/MainWindow.xaml.vb
+++ VB/GoldPrices/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Globalization
 Imports System.Windows
@@ -6,64 +6,70 @@ Imports System.Windows.Resources
 Imports System.Xml.Linq
 
 Namespace GoldPrices
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
+
         Private Function CreateDataSource() As List(Of GoldPrice)
-            Dim document As XDocument = DataLoader.LoadXmlFromResources("/Data/GoldPrices.xml")
-            Dim goldPrices As New List(Of GoldPrice)()
+            Dim document As XDocument = GoldPrices.DataLoader.LoadXmlFromResources("/Data/GoldPrices.xml")
+            Dim goldPrices As List(Of GoldPrice) = New List(Of GoldPrice)()
             If document IsNot Nothing Then
                 For Each element As XElement In document.Element("GoldPrices").Elements()
                     Dim [date] As Date = Convert.ToDateTime(element.Element("Date").Value, CultureInfo.InvariantCulture)
                     Dim price As Double = Convert.ToDouble(element.Element("Price").Value, CultureInfo.InvariantCulture)
                     goldPrices.Add(New GoldPrice([date], price))
-                Next element
+                Next
             End If
+
             Return goldPrices
         End Function
+
         Private Sub OnLoaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Me.DataContext = CreateDataSource()
+            DataContext = CreateDataSource()
         End Sub
     End Class
+
     Public Class GoldPrice
 
-        Private ReadOnly date_Renamed As Date
+        Private ReadOnly dateField As Date
 
-        Private ReadOnly price_Renamed As Double
-        Public ReadOnly Property [Date]() As Date
+        Private ReadOnly priceField As Double
+
+        Public ReadOnly Property [Date] As Date
             Get
-                Return date_Renamed
+                Return dateField
             End Get
         End Property
-        Public ReadOnly Property Price() As Double
+
+        Public ReadOnly Property Price As Double
             Get
-                Return price_Renamed
+                Return priceField
             End Get
         End Property
+
         Public Sub New(ByVal [date] As Date, ByVal price As Double)
-            Me.date_Renamed = [date]
-            Me.price_Renamed = price
+            dateField = [date]
+            priceField = price
         End Sub
     End Class
-    Public NotInheritable Class DataLoader
 
-        Private Sub New()
-        End Sub
+    Public Module DataLoader
 
-        Public Shared Function LoadXmlFromResources(ByVal fileName As String) As XDocument
+        Public Function LoadXmlFromResources(ByVal fileName As String) As XDocument
             Try
-                Dim uri As New Uri(fileName, UriKind.RelativeOrAbsolute)
+                Dim uri As Uri = New Uri(fileName, UriKind.RelativeOrAbsolute)
                 Dim info As StreamResourceInfo = Application.GetResourceStream(uri)
                 Return XDocument.Load(info.Stream)
             Catch
                 Return Nothing
             End Try
         End Function
-    End Class
+    End Module
 End Namespace
